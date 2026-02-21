eb146ac7579208d273b32d5be48d25470e673c0d
josoa-ramaroson <ramaroson.josoa.r@gmail.com>
2026-02-21 08:36:05 +0300

    feqt(git)[level 2] : 5. command for reverting changes in git

---
+++ git/level-2/5. Git revert some changes.sh
@@ -0,0 +1,7 @@
+# In /usr/src/kodekloudrepos/cluster git repository, 
+# revert the latest commit ( HEAD ) to the previous commit 
+cd /usr/src/kodekloudrepos/cluster
+git revert HEAD
+# (JFYI the previous commit hash should be with initial commit message ).
+# Use revert cluster message (please use all small letters for commit message) 
+# for the new revert commit.
