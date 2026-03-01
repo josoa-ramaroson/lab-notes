bff97999a15c69cd110289395ce40096ea0d9bf6
josoa-ramaroson <ramaroson.josoa.r@gmail.com>
2026-03-01 08:00:03 +0300

    feat(linux)[level 1]: 14. Default GUI Boot Configuration

---
+++ linux-lab/level 1/14. Default GUI Boot Configuration.sh
@@ -0,0 +1,7 @@
+# With the installation of new tools on the app servers within the Stratos Datacenter, certain functionalities now necessitate graphical user interface (GUI) access.
+
+
+# Adjust the default runlevel on all App servers in Stratos Datacenter to enable GUI booting by default. It's imperative not to initiate a server reboot after completing this task.
+
+# SSH into each server and execute the following command:
+sudo systemctl set-default graphical.target
