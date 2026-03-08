98663d654918bd46b054c8800689457482c75c47
josoa-ramaroson <ramaroson.josoa.r@gmail.com>
2026-03-08 22:52:06 +0300

    feat(linux) [level 2]: Adding the script for task 7

---
+++ linux-lab/level 2/7. Install packages.sh
@@ -0,0 +1,5 @@
+# As per new application requirements shared by the Nautilus project development team, serveral new packages need to be installed on all app servers in Stratos Datacenter. Most of them are completed except for strace.
+
+ssh <user>@<host>
+# Therefore, install the strace package on all app-servers.
+sudo yum install -y strace  
